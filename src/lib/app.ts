89809beef19314550af41eec8aa15988ee6dd48e
vitalspace <myglobalsounds@gmail.com>
2023-08-28 19:05:17 -0600

    added getCardBackgroundValue Foo

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -15,6 +15,12 @@ class App {
     backgroundImage.update((v) => (v = (e.target as HTMLInputElement).value));
   }
 
+  getCardBackgroundValue(e: Event) {
+    const cardColor = (e.target as HTMLInputElement).value;
+    const r = parseInt((e.target as HTMLInputElement).value.slice(1, 3), 16);
+    const g = parseInt((e.target as HTMLInputElement).value.slice(3, 5), 16);
+    const b = parseInt((e.target as HTMLInputElement).value.slice(5, 7), 16);
+  }
 }
 
 export { App };
